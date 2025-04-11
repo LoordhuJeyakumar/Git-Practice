7833c4433b563c4a315490b86640f103a55df880
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-04-11 12:11:18 +0530

    Refactor calculateProductPrice to include tax calculation based on region

--- pricing.js
+++ pricing.js
@@ -8,8 +8,25 @@ function calculateProductPrice(product) {
         basePrice = basePrice * (1 - product.discount / 100);// 200 * (1 - 10 / 100) = 180
     }
 
+    //Add tax based on region
+    let taxRate;
+    switch(product.region) {
+        case "US":
+            taxRate = 0.05;
+            break;
+        case "CA":
+            taxRate = 0.07;
+            break;
+        case "UK":
+            taxRate = 0.08;
+            break;
+        default:
+            taxRate = 0.05;
+            break;
+    }
+
     //Final price with tax
-    const finalPrice = basePrice * 1.15;// 180 * 1.15 = 198.0
+    const finalPrice = basePrice * (1 + taxRate); // 180 * (1 + 0.05) = 189
 
     return  finalPrice;
 
